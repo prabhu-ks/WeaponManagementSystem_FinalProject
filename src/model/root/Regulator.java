9ad68df618da4780b9736491a861da6cf7e79f85
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2022-12-02 16:13:51 -0500

    dealer commit

---
+++ src/model/root/Regulator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.root;
+
+/**
+ *
+ * @author manavmalavia
+ */
+public class Regulator extends Enterprise {
+    public Regulator(){
+        this.setEnterpriseType(EnterpriseType.Regulator);
+    }
+    
+}
