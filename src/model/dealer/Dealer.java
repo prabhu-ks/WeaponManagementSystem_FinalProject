1ecc92880365ed2e06eab67c48ee2f95037796e0
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2022-12-02 19:36:08 -0500

    Added connections

--- src/model/dealer/Dealer.java
+++ src/model/dealer/Dealer.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model.root;
+package model.dealer;
 
+import model.root.Enterprise;
 import model.root.Enterprise.EnterpriseType;
 
 /**
